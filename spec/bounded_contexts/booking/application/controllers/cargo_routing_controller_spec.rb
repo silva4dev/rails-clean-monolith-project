3d527f9f21acf3af1df34e415da9f5f90683a14d
silva4dev <web4dev.lucas@outlook.com>
2024-08-17 12:48:31 -0300

    test: 🧪 add cargo routing controller

---
+++ spec/bounded_contexts/booking/application/controllers/cargo_routing_controller_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+module Booking
+  module Application
+    module Controllers
+      RSpec.describe CargoRoutingController, type: :request do
+        let(:repo) { Infrastructure::Repositories::CargoRepository.new }
+        let(:create_cargo_params) {
+          {
+            booking_amount: 1,
+            origin_location: "ATH",
+            dest_location: "HAM",
+            dest_arrival_deadline: Time.now.iso8601
+          }
+        }
+        let(:create_cargo_request) { post "/bookings", params: create_cargo_params }
+        let(:do_request) { post "/cargorouting", params: { booking_id: cargo.booking_id.value } }
+        let(:cargo) do
+          create_cargo_request
+          repo.get_by_booking_id(JSON.parse(response.body)["value"])
+        end
+
+        it "responds with the correct http response" do
+          do_request
+          expect(response.status).to eq 200
+        end
+
+        it "creates update the cargo itinerary" do
+          do_request
+          updated = repo.get_by_booking_id(cargo.booking_id.value)
+
+          expect(updated.itinerary.legs).to be_present
+          expect(updated.itinerary.legs.size).to eq 2
+        end
+      end
+    end
+  end
+end
